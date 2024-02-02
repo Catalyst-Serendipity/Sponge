63a9e692a4bc5ed512153aed512df129c7a3db06
nicholass003 <veganicholas123@gmail.com>
2024-02-03 02:02:58 +0700

    chore: initialize project 🚀

---
+++ README.md
@@ -0,0 +1,7 @@
+## Sponge
+
+A PocketMine-MP plugin implementing sponge functionality for water absorption.
+
+## Credits
+
+<a target="_blank" href="https://icons8.com/icon/BADrGJ5K0bGW/sponge">sponge</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
---
+++ src/serendipity/sponge/Sponge.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Copyright (c) 2024 Catalyst-Serendipity
+ *      ______      __        __           __       _____                          ___       _ __       
+ *     / ____/___ _/ /_____ _/ /_  _______/ /_     / ___/___  ________  ____  ____/ (_)___  (_) /___  __
+ *    / /   / __ `/ __/ __ `/ / / / / ___/ __/_____\__ \/ _ \/ ___/ _ \/ __ \/ __  / / __ \/ / __/ / / /
+ *   / /___/ /_/ / /_/ /_/ / / /_/ (__  ) /_/_____/__/ /  __/ /  /  __/ / / / /_/ / / /_/ / / /_/ /_/ / 
+ *   \____/\__,_/\__/\__,_/_/\__, /____/\__/     /____/\___/_/   \___/_/ /_/\__,_/_/ .___/_/\__/\__, /  
+ *                          /____/                                                /_/          /____/   
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  Catalyst Serendipity Team
+ * @email   catalystserendipity@gmail.com
+ * @link    https://github.com/Catalyst-Serendipity
+ * 
+ */
+
+declare(strict_types=1);
+
+namespace serendipity\sponge;
+
+use pocketmine\block\Block;
+use pocketmine\block\Liquid;
+use pocketmine\block\Sponge as SpongeBlock;
+use pocketmine\block\VanillaBlocks;
+use pocketmine\block\Water;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\block\BlockSpreadEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Sponge extends PluginBase implements Listener{
+
+    protected function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onBlockSpread(BlockSpreadEvent $event) : void{
+        $source = $event->getSource();
+        $block = $event->getBlock();
+        $newState = $event->getNewState();
+        $targetBlocks = [$source, $block, $newState];
+        if($source instanceof Water){
+            foreach($targetBlocks as $targetBlock){
+                $this->findAndAbsorbWaterNearby($targetBlock);
+            }
+        }
+    }
+
+    public function onBlockPlace(BlockPlaceEvent $event) : void{
+        $blockAgainst = $event->getBlockAgainst();
+        $sponge = null;
+        foreach($event->getTransaction()->getBlocks() as [$x, $y, $z, $block]){
+            if($block instanceof SpongeBlock){
+                $sponge = $block;
+                break;
+            }
+        }
+        /** @var SpongeBlock $sponge */
+        $underSponeBlock = $sponge->getPosition()->getWorld()->getBlock($sponge->getPosition()->down());
+        if($sponge !== null && !$sponge->isWet()){
+            $spongeBlockPosition = $sponge->getPosition();
+            if($blockAgainst instanceof Water){
+                $this->absorbWater($sponge, $spongeBlockPosition->getX(), $spongeBlockPosition->getY(), $spongeBlockPosition->getZ());
+            }elseif(!$underSponeBlock->isSolid()){
+                $this->absorbWater($sponge, $spongeBlockPosition->getX(), $spongeBlockPosition->getY(), $spongeBlockPosition->getZ());
+            }
+            foreach($this->getNearBlocks($sponge) as $block){
+                if($block instanceof Water){
+                    $this->absorbWater($sponge, $spongeBlockPosition->getX(), $spongeBlockPosition->getY(), $spongeBlockPosition->getZ());
+                }
+            }
+        }
+    }
+
+    public function findAndAbsorbWaterNearby(Block $targetBlock) : void{
+        foreach($this->getNearBlocks($targetBlock) as $sponge){
+            if($sponge instanceof SpongeBlock && !$sponge->isWet()){
+                $spongeBlockPosition = $sponge->getPosition();
+                $this->absorbWater($sponge, $spongeBlockPosition->getX(), $spongeBlockPosition->getY(), $spongeBlockPosition->getZ());
+                break;
+            }
+        }
+    }
+
+    public function getNearBlocks(Block $block) : array{
+        $blocks = [];
+        $blockPosition = $block->getPosition();
+        $world = $blockPosition->getWorld();
+        $blocks[] = $world->getBlock($blockPosition->down());
+        $blocks[] = $world->getBlock($blockPosition->up());
+        $blocks[] = $world->getBlock($blockPosition->west());
+        $blocks[] = $world->getBlock($blockPosition->east());
+        $blocks[] = $world->getBlock($blockPosition->north());
+        $blocks[] = $world->getBlock($blockPosition->south());
+        return $blocks;
+    }
+
+    public function isWaterBlock(Block $block) : bool{
+        return ($block instanceof Liquid) ? true : false;
+    }
+
+    public function absorbWater(Block $sponge, float|int $spongeX, float|int $spongeY, float|int $spongeZ) : void{
+        /** @var SpongeBlock $sponge */
+        $absorbedWaterCount = 0;
+        $spongeBlockPosition = $sponge->getPosition();
+        for($x = $spongeX -7; $x <= $spongeX + 7; $x++){
+            for($y = $spongeY -7; $y <= $spongeY + 7; $y++){
+                for($z = $spongeZ -7; $z <= $spongeZ + 7; $z++){
+                    $targetBlock = $spongeBlockPosition->getWorld()->getBlockAt($x, $y, $z);
+                    if($this->isWaterBlock($targetBlock) &&
+                    (abs($x - $spongeX) + abs($y - $spongeY) + abs($z - $spongeZ)) <= 7){
+                        $sponge->getPosition()->getWorld()->setBlockAt($x, $y, $z, VanillaBlocks::AIR());
+                        $absorbedWaterCount++;
+                        if ($absorbedWaterCount >= 65) {
+                            break 3;
+                        }
+                    }
+                }
+            }
+        }
+        if($absorbedWaterCount > 0){
+            $sponge->setWet(true);
+        }
+        $spongeBlockPosition->getWorld()->setBlockAt($spongeBlockPosition->getX(), $spongeBlockPosition->getY(), $spongeBlockPosition->getZ(), $sponge);
+    }
+}
